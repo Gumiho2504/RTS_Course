b218abe7b076fe57e201167fa1a54010a0123988
Chanmetrey Hem <chanmetrey.hem@wingbank.com.kh>
2026-07-03 11:22:11 +0700

    feat(air transport): create unload all unit command

---
+++ Assets/Scripts/Commands/UnloadAllUnitsCommand.cs
@@ -0,0 +1,22 @@
+using Gumiho_Rts.Units;
+using UnityEngine;
+
+namespace Gumiho_Rts.Commands
+{
+    [CreateAssetMenu(fileName = "Unload All Units", menuName = "Units/Commands/Unload All Units", order = 107)]
+    public class UnloadAllUnitsCommand : BaseCommand
+    {
+        public override bool CanHandle(CommandContext context)
+        {
+            return context.Commandable is ITransporter transporter && transporter.Capacity > 0;
+        }
+
+        public override void Handle(CommandContext context)
+        {
+            ITransporter transporter = context.Commandable as ITransporter;
+            transporter.UnloadAll();
+        }
+
+        public override bool IsLocked(CommandContext context) => false;
+    }
+}
